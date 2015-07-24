60831d8138d08794a0bc85bc705b53b3404af3af
Aurelius Prochazka <aure@aure.com>
2015-07-23 23:12:31 -0700

    Removed the license from each individual file

--- Common/CsoundAUViewFactory.mm
+++ Common/CsoundAUViewFactory.mm
@@ -3,20 +3,6 @@
  *
  * Copyright (C) 2015 Edward Costello
  *
- * This software is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
 
 #import "CsoundAUViewFactory.h"
@@ -36,7 +22,7 @@
 
 - (NSView *)uiViewForAudioUnit:(AudioUnit)inAU withSize:(NSSize)inPreferredSize
 {
-    
+
     NSBundle *bundle = [NSBundle bundleWithIdentifier:@BUNDLEID];
     NSString *bundlePath = [[[[bundle bundlePath]
                               stringByDeletingLastPathComponent]
@@ -51,13 +37,13 @@
         NSLog (@"Unable to load nib for view.");
         return nil;
     }
-    
+
     uiFreshlyLoadedView.auBundlePath = [NSString stringWithCString:auBundlePath.c_str() encoding:NSUTF8StringEncoding];
     [uiFreshlyLoadedView setAU:inAU];
-    
+
     NSView *returnView = uiFreshlyLoadedView;
     uiFreshlyLoadedView = nil;
-    
+
     return returnView;
 }
 
--- Common/Parameter.h
+++ Common/Parameter.h
@@ -3,20 +3,6 @@
  *
  * Copyright (C) 2015 Edward Costello
  *
- * This software is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
 
 #import <string>
@@ -32,7 +18,7 @@ public:
               Float32 defaultValue,
               AudioUnitParameterUnit unit,
               UInt32 flags);
-    
+
     Parameter(string name,
               Float32 minValue,
               Float32 maxValue,
@@ -40,7 +26,7 @@ public:
               AudioUnitParameterUnit unit,
               UInt32 flags,
               vector <string> strings);
-    
+
     string name;
     Float32 minValue;
     Float32 maxValue;
